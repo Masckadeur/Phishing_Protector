aed8b892b2a226a81ca4e32330cbc9009021fc91
Erwan Benard <erwan.benard@epita.fr>
2023-04-20 20:03:03 +0200

    remise à jour, modification du fonctionnement

--- script/collect_mail.py
+++ script/collect_mail.py
@@ -1,8 +1,13 @@
 import imaplib
-import pprint
+import email
+from email.header import decode_header
+import os
+import analyse_emmeteur as ae
+import analyse_extension as aex
 
-print("entré 1 pour l'utilisation par l'host par defaut : outlook.office365.com")
-print("entré 2 pour utiliser un autre host")
+
+print("entrez 1 pour l'utilisation par l'host par defaut : outlook.office365.com")
+print("entrez 2 pour utiliser un autre host")
 host = input()
 
 
@@ -15,9 +20,9 @@ if host > '1' :
 line = []
 print("utilisation d'un fichier credentials ? (O/N)")
 if input() == 'N' :
-    print("entré votre email :")
+    print("entrez votre email :")
     line.append(input())
-    print("entré votre mot de passe :")
+    print("entrez votre mot de passe :")
     line.append(input())
 else :
     with open('credentials.txt', 'r') as f :
@@ -25,25 +30,61 @@ else :
             line.append((l.rstrip('\n')))
         f.close()
     
-imap_user = line[0]
-imap_pass = line[1]
+username = line[0]
+password = line[1]
 
-# connect to host using SSL
 imap = imaplib.IMAP4_SSL(imap_host)
+## authenticate
+imap.login(username, password)
+print("\nLongin sucessful")
 
-## login to server
-imap.login(imap_user, imap_pass)
+status, messages = imap.select("Inbox")
 
-print("veuillé faire voter choix de mailbox.\n par defaut \"Inbox\"")
-print("entré votre boite au lettre : ")
-imap.select(input())
-#imap.select('Inbox')
+N = int(input("Entrez le nombre de mail à récupérer :\n"))
+print("-"*100)
 
-tmp, data = imap.search(None, 'ALL')
-for num in data[0].split():
-    tmp, data = imap.fetch(num, '(RFC822)')
-    print('Message: {0}\n'.format(num))
-    pprint.pprint(data[0][1])
-    break
+messages = int(messages[0])
+###
+for i in range(messages, messages-N, -1):
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        
+        if isinstance(response, tuple):
+            msg = email.message_from_bytes(response[1])
+            subject = decode_header(msg["Subject"])[0][0]
+            if isinstance(subject, bytes):
+                subject = subject.decode('latin-1')
+            
+            print("Subject:", subject)
+            emetteur = msg.get("From") 
+            print("From:", emetteur)
+            if not ae.verif(emetteur):	# check émetteur parmis une liste ban (pour les test adresse email erwan.benard@epita.fr et *@epita.fr)
+            	print("émetteur interdit")
+
+            if msg.is_multipart():
+                for part in msg.walk():
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    try:
+                        body = part.get_payload(decode=True).decode('latin-1')
+                    except:
+                        pass
+                    if content_type == "text/html" and "attachment" not in content_disposition:
+                        index = str(part).find('<')
+                        body = str(part)[index:]
+                    elif content_type == "text/plain" and "attachment" not in content_disposition:
+                        print(body)
+                    elif "attachment" in content_disposition:
+                        filename = part.get_filename()
+                        if not aex.verif(filename.split('.')[-1]):
+                            print(filename, " !!! fichier interdit") #Vérifie si l'extension est autorisé
+
+                        
+                        if filename:
+                            filepath = os.path.join('.', 'pieces_jointes', filename)
+                            open(filepath, "wb").write(part.get_payload(decode=True))
+
+            print("="*100)
 
 imap.close()
+imap.logout()
