8a82c73f3cdb25dce56f843c4e710d3a19225586
Clément Durin <durin.xxxxxxx@gmail.com>
2023-04-20 19:32:12 +0200

    fonction pour les href fraudulentes

--- script/analyse_mail_html.py
+++ script/analyse_mail_html.py
@@ -1,13 +1,31 @@
 from bs4 import BeautifulSoup as sp
+import url_finder
 
-f = open('texte.txt', 'r')
+f = open('texte2.txt', 'r')
 html_doc = f.read()
 f.close()
-
+f = open('ban/domaine.txt')
+bannedDomain = f.read().split('\n')
+f.close()
 soup = sp(html_doc, 'html.parser')
 
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+def analyse_redirection_fraudulente(soup):
+    for link in soup.find_all('a'):
+        vis = url_finder.Find(str(link))
+        if(len(vis)>0):
+            if(len(vis) != vis.count(vis[0])):
+                return False
+            for ban in bannedDomain:
+                if(ban in vis[0]):
+                    return False
+    return True
 
-print(soup.find_all('a'))
+print(analyse_redirection_fraudulente(soup))
+
+
+# href
+# redirection fraudulente
+
+
+#print(soup.find_all('a'))
